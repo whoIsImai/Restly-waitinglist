ada6ae7b4fd38c251ee52f9c76504b50b5ae51d8
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-05-10 22:45:01 +0200

    added a config file for firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+export const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+}
